903ee55af69d161089240dc94db836a8bdf1537e
Hafnium <haf@hafnium.me>
2022-12-29 01:44:53 +0100

    app.py: More documentation and code cleanup

--- app.py
+++ app.py
@@ -3,16 +3,23 @@ import pandas as pd
 
 # Load OpenAI Whisper base model
 model = whisper.load_model("base")
+
+# I got no idea of what this has to do with 'temperature',
+#  but it transcribes more precisely
+# Borrowed from here: https://huggingface.co/spaces/Finnish-NLP/Whisper-ASR-youtube-subtitles/blob/main/app.py#L28
+# See https://github.com/openai/whisper/blob/main/whisper/transcribe.py#L264
 transcribe_options = dict(beam_size=3, best_of=3, without_timestamps=False)
 
 video = "import-4192-eng-This_years_badge_hd.mp4"
 
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio(video)
-audio = whisper.pad_or_trim(audio)
+# ToDo: Fix text in start of video when there is no talking
+def transcribe_audio(video_file_path: str, transcribe_options) -> pd.DataFrame:
+    """
+    Load file and process the audio
 
-# Uses this for now: Whisper-ASR-youtube-subtitles: https://huggingface.co/spaces/Finnish-NLP/Whisper-ASR-youtube-subtitles
-def speech_to_text(video_file_path, transcribe_options):
+    Transcribe audio to create dataframes with start time, end time and text
+    Uses this for now: Whisper-ASR-youtube-subtitles: https://huggingface.co/spaces/Finnish-NLP/Whisper-ASR-youtube-subtitles
+    """
 
     if(video_file_path == None):
         raise ValueError("Error no video input")
@@ -21,7 +28,6 @@ def speech_to_text(video_file_path, transcribe_options):
         audio = whisper.load_audio(video_file_path)
     except Exception as e:
         raise RuntimeError("Error converting video to audio")
-
     try:
         print(f'Transcribing via local model')
 
@@ -35,66 +41,62 @@ def speech_to_text(video_file_path, transcribe_options):
             new_row = {'start': segment['start'],
             'end': segment['end'],
             'text': segment['text']
-                            }
-            # df = df.append(new_row, ignore_index=True)
+                        }
             df = pd.concat([df, pd.DataFrame([new_row])], axis=0, ignore_index=True)
 
         return (df)
     except Exception as e:
-        raise RuntimeError("Error Running inference with local model", e)
+        raise RuntimeError("Error running inference with local model", e)
 
+def time_calc(milliseconds) -> str:
+    """
+    Calculate start and end time from text and output in SubRip time format
+    """
 
-# ToDo: Look into srt library
-# ToDo: Fix text in start of video when there is no talking
-def create_srt(df):
+    hours = milliseconds // 3_600_000
+    milliseconds -= hours * 3_600_000
 
-    print("Starting creation of video wit srt")
+    minutes = milliseconds // 60_000
+    milliseconds -= minutes * 60_000
 
-    with open('test.srt','w', encoding="utf-8") as file:
+    seconds = milliseconds // 1_000
+    milliseconds -= seconds * 1_000
+
+    return f"{hours}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
+
+def create_srt(df: pd.DataFrame, video: str):
+    """
+    Format dataframes to SRT file
+    SubRip file format information: https://en.wikipedia.org/wiki/SubRip
+    """
+
+    print("Starting creation SRT file")
+    with open(f'{video}.srt','w', encoding="utf-8") as file:
         for i in range(len(df)):
+            # Set index
             file.write(str(i+1))
             file.write('\n')
+
+            # Set text start time
             start = df.iloc[i]['start']
-
-
             milliseconds = round(start * 1000.0)
+            time_format = time_calc(milliseconds)
+            file.write(time_format)
 
-            hours = milliseconds // 3_600_000
-            milliseconds -= hours * 3_600_000
-
-            minutes = milliseconds // 60_000
-            milliseconds -= minutes * 60_000
-
-            seconds = milliseconds // 1_000
-            milliseconds -= seconds * 1_000
-
-            file.write(f"{hours}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}")
-
+            # Set text end time
             stop = df.iloc[i]['end']
-
-
             milliseconds = round(stop * 1000.0)
-
-            hours = milliseconds // 3_600_000
-            milliseconds -= hours * 3_600_000
-
-            minutes = milliseconds // 60_000
-            milliseconds -= minutes * 60_000
-
-            seconds = milliseconds // 1_000
-            milliseconds -= seconds * 1_000
-
-
+            time_format = time_calc(milliseconds)
             file.write(' --> ')
-            file.write(f"{hours}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}")
+            file.write(time_format)
+
+            # Insert text
             file.write('\n')
             file.writelines(df.iloc[i]['text'])
             if int(i) != len(df)-1:
                 file.write('\n\n')
 
-    print("SRT DONE")
+    print("Subtitles have been finished")
 
-spt = speech_to_text(video, transcribe_options)
-apply_subtitle = create_srt(spt)
-# print(spt)
-# print(apply_subtitle)
+transcribe = transcribe_audio(video, transcribe_options)
+subtitles_creation = create_srt(transcribe, video)
